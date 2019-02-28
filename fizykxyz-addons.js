c3ade3078fc53fc390b192ef610977f4ea19f012
fizykxyz <40587518+fizykxyz@users.noreply.github.com>
2019-02-28 23:50:35 +0100

    Update fizykxyz-addons.js

--- fizykxyz-addons.js
+++ fizykxyz-addons.js
@@ -3,6 +3,6 @@ function dec2bin(dec,places)
 {
 	places = Math.floor(places);
 	var result = parseInt((dec >>> 0), 10).toString(2); //(dec >>> 0).toString(2);
-	return (places >= result.length) ? _s.repeat('0', places - result.length) + result : '#NUM!';
+	return (places >= result.length) ? '0'.repeat(places - result.length) + result : '#NUM!';
 }
 ;

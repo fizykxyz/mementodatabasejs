9b236d7644e84a5c6da61f04387d88338537e3c3
fizykxyz <40587518+fizykxyz@users.noreply.github.com>
2019-02-28 23:42:20 +0100

    Update fizykxyz-addons.js
    dodano ilość bitów

--- fizykxyz-addons.js
+++ fizykxyz-addons.js
@@ -1,5 +1,8 @@
 ;
-function dec2bin(dec){
-    return (dec >>> 0).toString(2);
+function dec2bin(dec,places)
+{
+	places = Math.floor(places);
+	var result = parseInt((dec >>> 0), 10).toString(2); //(dec >>> 0).toString(2);
+	return (places >= result.length) ? _s.repeat('0', places - result.length) + result : '#NUM!';
 }
 ;

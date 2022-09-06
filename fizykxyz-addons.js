2a3af362d65f8b6cecc6c888f6ddc53418adcd97
fizykxyz <40587518+fizykxyz@users.noreply.github.com>
2022-09-06 11:02:53 +0200

    Update fizykxyz-addons.js

--- fizykxyz-addons.js
+++ fizykxyz-addons.js
@@ -5,4 +5,8 @@ function dec2bin(dec,places)
 	var result = parseInt((dec >>> 0), 10).toString(2); //(dec >>> 0).toString(2);
 	return (places >= result.length) ? '0'.repeat(places - result.length) + result : '#NUM!';
 }
+function potega(podstawa,wykladnik)
+{
+	return podstawa^wykladnik
+}
 ;

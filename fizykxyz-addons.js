960dc3d70b3a5380097d845aed39b0731caab138
fizykxyz <40587518+fizykxyz@users.noreply.github.com>
2024-08-12 13:37:45 +0200

    Update fizykxyz-addons.js

--- fizykxyz-addons.js
+++ fizykxyz-addons.js
@@ -1,24 +1,26 @@
-;
-function dec2bin(dec,places)
+//https://replit.com/@TomiDab/mementodbjs#script.js
+function dec2bin(dec, places) 
 {
-	places = Math.floor(places);
-	var result = parseInt((dec >>> 0), 10).toString(2); //(dec >>> 0).toString(2);
-	return (places >= result.length) ? '0'.repeat(places - result.length) + result : '#NUM!';
+  places = Math.floor(places);
+  var result = parseInt((dec >>> 0), 10).toString(2); //(dec >>> 0).toString(2);
+  return (places >= result.length) ? '0'.repeat(places - result.length) + result : '#NUM!';
 }
-function potega(podstawa,wykladnik)
+
+function potega(podstawa, wykladnik)
 {
-	return (podstawa)^(wykladnik);
+  return (podstawa) ^ (wykladnik);
 }
-function generujEtykiete()
+
+function rokPrzestepny(rok)
 {
-let label="svg width=";
-
+  return ((rok % 4 == 0) && ((rok % 100 != 0) || (rok % 400 == 0)));
 }
-var rec = entry();
-var da = rec.field("da");
-var a = rec.field("RTF");	
 
+//var rec = entry();
+//var da = rec.field("da");
+//var a = rec.field("RTF");	
 
+/*
 var calData = { 
   daysName:   ['Poniedziałek','Wtorek','Środa','Czwartek','Piątek','Sobota','Niedziela'],
   daysShort:  ['Pn','Wt','Śr','Cz','Pi','So','Ni'],
@@ -26,84 +28,116 @@ var calData = {
   monthName:  ['Styczeń','Luty','Marzec','Kwiecień','Maj','Czerwiec','Lipiec','Sierpień','Wrzesień','Październik','Listopad','Grudzień'],
   monthShort: ['St','Lu','Mr','Kw','Mj','Czerwiec','Lipiec','Sierpień','Wrzesień','Październik','Listopad','Grudzień'],
 }
+*/
 
-
-function rokPrzestepny(rok)
-{
-  return ((rok % 4 == 0) && ((rok % 100 != 0) || (rok % 400 == 0)));
-}
 
 function kalendarz()
 {
-  data = new Date();
+  var data = new Date();
   var rok = data.getYear();
   if (rok < 1000) rok = 2000 + rok - 100;
   var miesiac = data.getMonth();
   var dzienTygodnia = data.getDay();
   var dzienMiesiaca = data.getDate();
   var tempDate = new Date(rok, miesiac, 1);
-  var pierwszyDzienMiesiaca = tempDate.getDay();
-  var kal="<b>Kalendarz</b><br>";
-  if(dzienTygodnia == 0) dzienTygodnia = 7;
-  if(pierwszyDzienMiesiaca == 0) pierwszyDzienMiesiaca = 7;
-  var nazwyMies=['Styczeń','Luty','Marzec','Kwiecień','Maj','Czerwiec','Lipiec','Sierpień','Wrzesień','Październik','Listopad','Grudzień']
-  var dniMies=[31,28+rokPrzestepny(rok),31,30,31,30,31,31,30,31,30,31];
-  var nazwaMiesiaca=nazwyMies[miesiac];
-  var dniWMiesiacu=dniMies[miesiac];
-  kal+="<TABLE border = 1><TR>";
-  kal+="<TD bgcolor='yellow' align='center' colspan='7'>";
-  kal+=nazwaMiesiaca + " " + rok;
-  kal+="</TD></TR><TR>";
+  var pierwszyDzienMiesiaca = tempDate.getDay();  
+  if (dzienTygodnia == 0) dzienTygodnia = 7;
+  if (pierwszyDzienMiesiaca == 0) pierwszyDzienMiesiaca = 7;
+  var nazwyMies = ['Styczeń', 'Luty', 'Marzec', 'Kwiecień', 'Maj', 'Czerwiec', 'Lipiec', 'Sierpień', 'Wrzesień', 'Październik', 'Listopad', 'Grudzień']
+  var dniMies = [31, 28 + rokPrzestepny(rok), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  var nazwyDni = ['Pon','Wt','Śr','Cz','Pi','So','Ni'];
+  var roboczy = '#D3D3D3';
+  var wolny = '#FF6550';
+  var aktualny = '#FFFF00';
+  var koloryDni = [roboczy,roboczy,roboczy,roboczy,roboczy,wolny,wolny];
+  var przyciemnijDni = -30;
+  var nazwaMiesiaca = nazwyMies[miesiac];
+  var dniWMiesiacu = dniMies[miesiac];
+  
+  var kal = "<DIV class='calendar'> <b>Kalendarz</b><br>";
+  kal += "<TABLE border = 1><TR>";
+  kal += "<TD bgcolor='yellow' align='center' colspan='7'>";
+  kal += nazwaMiesiaca + " " + rok;
+  kal += "</TD></TR><TR>";
 
-  kal+="<TR>";
-  kal+="<TD align='center' bgcolor='pink'>Pn</TD>";
-  kal+="<TD align='center' bgcolor='pink'>Wt</TD>";
-  kal+="<TD align='center' bgcolor='pink'>Śr</TD>";
-  kal+="<TD align='center' bgcolor='pink'>Cz</TD>";
-  kal+="<TD align='center' bgcolor='pink'>Pi</TD>";
-  kal+="<TD align='center' bgcolor='pink'>So</TD>";
-  kal+="<TD align='center' bgcolor='pink'>Nd</TD>";
-  kal+="</TR>";
+  kal += "<TR>";
+  for (var i=0;i<7;i++)
+    {
+      kal += "<TD align='center' bgcolor='"+koloryDni[i]+"'>"+nazwyDni[i]+"</TD>";
+    }
+  kal += "</TR>";
 
-var j = dniWMiesiacu + pierwszyDzienMiesiaca - 1;
+  var j = dniWMiesiacu + pierwszyDzienMiesiaca - 1;
 
-  for(var i = 0; i < j; i++){
-    if(i < pierwszyDzienMiesiaca - 1){
-      kal+="<TD bgcolor='white'></TD>";
+  for (var i = 0; i < j; i++) {
+    if (i < pierwszyDzienMiesiaca - 1) {
+      kal += "<TD bgcolor='white'></TD>";
       continue;
     }
-    if((i % 7) == 0){
-      kal+="</TR><TR>";
+    if ((i % 7) == 0) {
+      kal += "</TR><TR>";
     }
-    if((i - pierwszyDzienMiesiaca + 2) == dzienMiesiaca){
-      color = "yellow";
+    if ((i - pierwszyDzienMiesiaca + 2) == dzienMiesiaca) {
+      color = aktualny;
     }
-    else{
-      color = "green";
+    else {
+      color = LightenDarkenColor(koloryDni[i%7],przyciemnijDni);
     }
-    kal+="<TD bgcolor='" + color + "' align='center'>";
-    kal+=i - pierwszyDzienMiesiaca + 2;
-    kal+="</TD>";
+    kal += "<TD bgcolor='" + color + "' align='center'>";
+    kal += i - pierwszyDzienMiesiaca + 2;
+    kal += "</TD>";
   }
-  kal+="</TR></TABLE>";
-
-return kal;
+  kal += "</TR></TABLE></DIV>";
+  return kal;
 }
 
-function startTime() {
+
+function startTime()
+{
     var today = new Date();
     var h = today.getHours();
     var m = today.getMinutes();
     var s = today.getSeconds();
-    m = checkTime(m);
-    s = checkTime(s);
-    //var t = setTimeout(startTime, 500);    
-    return    h + ":" + m + ":" + s; 
+    m = leadingZero(m);
+    s = leadingZero(s);
+    //var t = setTimeout(startTime, 500);
+    return    h + ":" + m + ":" + s;
 }
-function checkTime(i) {
+
+function leadingZero(i)
+{
     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
     return i;
 }
+
+function LightenDarkenColor(col,amt)
+{
+    var usePound = false;
+    if ( col[0] == "#" ) {
+        col = col.slice(1);
+        usePound = true;
+    }
+
+    var num = parseInt(col,16);
+
+    var r = (num >> 16) + amt;
+
+    if ( r > 255 ) r = 255;
+    else if  (r < 0) r = 0;
+
+    var b = ((num >> 8) & 0x00FF) + amt;
+
+    if ( b > 255 ) b = 255;
+    else if  (b < 0) b = 0;
+
+    var g = (num & 0x0000FF) + amt;
+
+    if ( g > 255 ) g = 255;
+    else if  ( g < 0 ) g = 0;
+
+    return (usePound?"#":"") + (g | (b << 8) | (r << 16)).toString(16);
+}
+
 
 //var kal="<b>Kalendarz</b>";
 //kal+="<b>test</b>";
@@ -111,6 +145,10 @@ function checkTime(i) {
 
 //kal+="<b>test</b> <tableta za border='1'><tr><th>Firstname</th><th>Lastname</t to takieh><th>Age</th></tr><tr><td>Jill</td><td>Smith</td><td>50</td></tr><tr><td>Eve</td><td>Jackson</td><td>94</td></tr><tr><td>Joh</td><td>Doe</td><td>80</td></tr></table>");
 
-rec.set("RTF", kalendarz());
+//rec.set("RTF", kalendarz());
 
-;
+//document.body.innerHTML += kalendarz();
+//message(kalendarz());
+//alert(kalendarz());
+
+
